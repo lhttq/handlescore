68e5bbde84d71d90e9cb273420af98f886bb749b
lht <lhttq19970721@163.com>
2021-03-05 14:28:38 +0800

    测试vue3.0中的this

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,65 @@
 <template>
   <div class="hello">
     <div class="container" ref="container">
-      hahah
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="活动时间">
+          <el-col :span="11">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="form.date1"
+              style="width: 100%;"
+            ></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="form.date2"
+              style="width: 100%;"
+            ></el-time-picker>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="即时配送">
+          <el-switch v-model="form.delivery"></el-switch>
+        </el-form-item>
+        <el-form-item label="特殊资源">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="线上品牌商赞助"></el-radio>
+            <el-radio label="线下场地免费"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="活动形式">
+          <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-
-@Options({
-  props: {
-    msg: String
-  }
-})
+import { Vue } from "vue-class-component";
+import Form from "@/models/Form";
 export default class HelloWorld extends Vue {
-  msg!: string;
+  private form: Form = new Form();
   mounted(): void {
     console.log(this.$refs.container);
+  }
+  private onSubmit() {
+    console.log((this.$refs.form as any).validate());
   }
 }
 </script>
---
+++ src/models/Form.ts
@@ -0,0 +1,10 @@
+export default class {
+  public name = "";
+  public region = "";
+  public date1 = "";
+  public date2 = "";
+  public delivery = false;
+  public type = [];
+  public resource = "";
+  public desc = "";
+}
